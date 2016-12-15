40bb57d1a43fe238b75bc2985a73ea2ddcb6e450
shyreck <shyreckdc@sjtu.edu.cn>
2016-12-15 09:27:37 +0800

    test .gitignore with Demo7

---
+++ cmake/cmake-demo-master/Demo7/config.h
@@ -0,0 +1,8 @@
+// the configured options and settings for Tutorial
+#define Demo_VERSION_MAJOR 1
+#define Demo_VERSION_MINOR 0
+
+// does the platform provide pow function?
+/* #undef HAVE_POW */
+/* #undef HAVE_LOG */
+/* #undef HAVE_EXP */
